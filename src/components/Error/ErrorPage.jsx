8980318dea9cea14bd8636f0471c9462b43e863c
Thomas Vallat <vallat.thomas.pro@gmail.com>
2024-07-15 05:04:57 +0200

    Création de la page d'erreur

---
+++ src/components/Error/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Error.scss';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <section>
+            <h2 className='error-title'>404</h2>
+            <p className='error-text'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="nav-link" to="/">Retourner sur la page d'accueil</Link>
+        </section>
+    );
+};
+
+export default ErrorPage;
